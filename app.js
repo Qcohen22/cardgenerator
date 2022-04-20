fc4a42431f58632a43d6359f613bb71bac3a54bd
Qcohen22 <cohenjr.quincy@live.jobcorps.org>
2022-04-20 16:23:48 +0000

    cardgenerator

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ let spade = [1,2,3,4,5,6,7,8,9,10,"K", "Q", "J","A"]
 let arr = spade[Math.floor(Math.random()*spade.length)]
 let suit = ["♦","♥","♠","♣"]
 let icon = suit[Math.floor(Math.random()*suit.length)]
- document.querySelector("#cards").innerHTML = arr
+if(icon==="♥") {
+    document.querySelector("#icon").style.color="red"
+}
+if(icon==="♥") {
+    document.querySelector("#iconTwo").style.color="red"
+}
+document.querySelector("#cards").innerHTML = arr
 document.querySelector("#icon").innerHTML = icon
 document.querySelector("#iconTwo").innerHTML = icon
